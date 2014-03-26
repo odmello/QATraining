bc4d16b39b1a2e58dada633f805bf52d6064b5a2
odmello <oscar@boats.com>
2014-03-26 10:42:56 -0700

    SQL statements for FSBO database
    These are the SQL statements I use when working with the FSBO databases
    QA/Prod
    - save the file
    - select FSBO db
    - load SQL file

---
+++ FSBO ads Queries.sql
@@ -0,0 +1,6 @@
+select * from ads order by id desc;
+select * from job_journal order by id desc;
+select * from users order by id desc;
+select * from email_queue order by id desc;
+select * from destinations order by id desc;
+select * from boats order by id desc;
